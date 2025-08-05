9b522c1265e08080501f77de2d27da10838ba8be
Kalaivanan <kalaivanankarthi1531@gmail.com>
2025-08-05 09:25:08 +0530

    change app function

--- task/src/App.jsx
+++ task/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Work from "./components/Form";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <Work />
+      <From />
       
     </div>
   );
