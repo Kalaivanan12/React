0b4c493541b08e611fe761853f60481edc9eb312
Kalaivanan <kalaivanankarthi1531@gmail.com>
2025-10-14 10:28:33 +0530

    add one more

---
+++ livepro/my-backend/exp.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 2831;
+
+let users = [
+  { id: 1, name: "Kalai", email: "kalai@gmail.com" },
+  { id: 2, name: "Vijay", email: "vijay@gmail.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/write", (req, res) => {
+  const { id, name, email } = req.query;
+
+  if (!id || !name || !email) {
+    return res.status(400).send("Missing id, name, or email parameters!");
+  }
+
+  
+  const exists = users.some((u) => u.id === parseInt(id));
+  if (exists) {
+    return res.status(400).send("User ID already exists!");
+  }
+
+  const newUser = { id: parseInt(id), name, email };
+  users.push(newUser);
+
+  res.send(`User added successfully!<br><br>
+    <b>ID:</b> ${id}<br>
+    <b>Name:</b> ${name}<br>
+    <b>Email:</b> ${email}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
