d75056fc68413e2deff7eb20d7102ed32f903769
Kalaivanan <kalaivanankarthi1531@gmail.com>
2025-10-24 10:27:36 +0530

    change frontend

---
+++ my-project/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ValidationForm from "./components/ValidationForm";
+import "./App.css"; // optional if you want global styles
+
+function App() {
+  return (
+    <div className="app-container">
+      <ValidationForm />
+    </div>
+  );
+}
+
+export default App;
